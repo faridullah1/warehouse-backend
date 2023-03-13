1d3a911cf940c39cbe33f1c2921b9d176c513e3f
faridullah1 <faridullah996@gmail.com>
2023-03-13 22:52:10 +0500

    seed file added for intial data

---
+++ seed.js
@@ -0,0 +1,64 @@
+const dotEnv = require('dotenv');
+const bcrypt = require('bcrypt');
+
+dotEnv.config({ path: './config.env'});
+require('./associations')();
+
+const sequelize = require('./db');
+await = sequelize.sync();
+
+const { Department } = require('./models/departmentModel');
+const { User } = require('./models/userModel');
+
+const counts = 2;
+
+createDepartments = async () => {
+    for (let i=0; i<counts; i++) {
+        await Department.create({ name: `Department ${i+1}`});
+    }
+}
+
+createSuperAdmins = async () => {
+    for (let i=0; i<counts; i++) {
+        const salt = await bcrypt.genSalt(10);
+        const encryptedPassword = await bcrypt.hash('pakistan', salt);
+
+        const user = { 
+            name: `Admin ${i+1}`, 
+            email: `admin${i+1}@gmail.com`, 
+            password: encryptedPassword, 
+            type: 'Super_Admin', 
+            departmentId: i+1 
+        };
+
+        await User.create(user);
+    }
+}
+
+createWarehousePersonnel = async () => {
+    for (let i=0; i<counts; i++) {
+        const salt = await bcrypt.genSalt(10);
+        const encryptedPassword = await bcrypt.hash('pakistan', salt);
+
+        const user = { 
+            name: `User ${i+1}`, 
+            email: `user${i+1}@gmail.com`, 
+            password: encryptedPassword, 
+            type: 'Warehouse_Personnel', 
+            departmentId: i+1
+        };
+
+        await User.create(user);
+    }
+}
+
+initData = async () => {
+    await createDepartments();
+    console.log(`${counts} departments created...`);
+    await createSuperAdmins();
+    console.log(`${counts} super admins created...`);
+    await createWarehousePersonnel();
+    console.log(`${counts} warehouse personnel created...`);
+}
+
+initData();
