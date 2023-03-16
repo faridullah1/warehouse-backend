60bf2d1bcbd9754e8c26470e327cc964c3f02192
faridullah1 <faridullah996@gmail.com>
2023-03-16 15:07:36 +0500

    Database shifted to postgres as Render deployment support it.

--- db.js
+++ db.js
@@ -1,12 +1,8 @@
 const Sequelize = require('sequelize');
+
 const connection = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
-	define: {
-		charset: 'utf8',
-		collate: 'utf8_general_ci', 
-		timestamps: true
-	},
-	dialect: 'mysql',
-	host: process.env.HOST,
+	dialect: 'postgres',
+	host: process.env.HOST
 });
 
 module.exports = connection;
